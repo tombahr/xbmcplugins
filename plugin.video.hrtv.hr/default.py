6da9e48810361f29e0a29f5af64b6d87db8325e8
tombahr <trajnovic@gmail.com>
2014-05-17 19:30:20 +0200

    Update default.py

--- plugin.video.hrtv.hr/default.py
+++ plugin.video.hrtv.hr/default.py
@@ -3,7 +3,6 @@ import urllib
 import urlparse
 import xbmcgui
 import xbmcplugin
-from HTMLParser import HTMLParser
 import re
 
 base_url = sys.argv[0]
@@ -17,59 +16,43 @@ def build_url(query):
 
 mode = args.get('mode', None)
 
-#
-#parser
-class MyHTMLParser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		# Only parse the 'anchor' tag.
-		if tag == "a":
-			# Check the list of defined attributes.
-			take = False
-			title=""
-			link =""
-			for name, value in attrs:
-				# If href is defined, print it.				
-				if name == "href":
-					if rex.match(value):
-						take = True
-						link = value
-				elif name == "title" and take:
-					title = value
-			if take and not title == "":
-				#print title, " : ", link
-				nl[title]=link
 
-#
 
 nl = {}
-
 rtllink = "http://www.rtl.hr"
 
+
 if mode is None:
-	url = build_url({'mode': 'rtl', 'foldername': 'RTL sada', 'link':'/rtl-sada/'})
+	url = build_url({'mode': 'rtl', 'foldername': 'RTL sada', 'link':'/rtl-sada'})
 	li = xbmcgui.ListItem('RTL sada', iconImage='DefaultFolder.png')
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)	
 	xbmcplugin.endOfDirectory(addon_handle)
 	
 # RTL sada
+
 elif mode[0] == 'rtl':
 	foldername = args['foldername'][0]
 	link = args['link'][0]
 	
-	rex = re.compile(link+'[\w-]+/$');
-	parser = MyHTMLParser()
-	parser.feed(urllib.urlopen(rtllink+link).read())
+	###1 folder
+	#link = '/rtl-sada'
+	pat = r'<a href="('+link+'/[\w-]*)/" title="([\w\s-]*)">.*</a>'
+	doc = urllib.urlopen(rtllink+link).read()
+	nl = re.findall( pat, doc, re.I)
 	
+	nl = list(set(nl))
+	#print matchobj
 	for n in nl:
-		url = build_url({'mode': 'rtl', 'foldername': n, 'link': nl[n]})
-		li = xbmcgui.ListItem(n, iconImage='DefaultFolder.png')
+		url = build_url({'mode': 'rtl', 'foldername': n[1], 'link': n[0]})
+		li = xbmcgui.ListItem(n[1], iconImage='DefaultFolder.png')
 		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
-		
-	rex = re.compile(link+'(\d*)/.*/$');	
-	parser.feed(urllib.urlopen(rtllink+link).read())
+	
+	#2 xml
+	pat = '<a href="('+link+'/[\d]*/.*/)" title="(.*)">'
+	nl = re.findall( pat, doc, re.I)
 	for n in nl:
-		url = build_url({'mode': 'rtlxml', 'foldername': n, 'link': nl[n]+"?xml=1"})
-		li = xbmcgui.ListItem(n, iconImage='DefaultFolder.png')
+		url = build_url({'mode': 'rtlxml', 'foldername': n[1], 'link': n[0]+"?xml=1"})
+		li = xbmcgui.ListItem(n[1], iconImage='DefaultFolder.png')
 		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 		
 	xbmcplugin.endOfDirectory(addon_handle)
@@ -86,8 +69,7 @@ elif mode[0] == 'rtlxml':
 	#url = build_url({'mode': 'rtl', 'foldername': 'RTL sada', 'link':'/rtl-sada/'})
 	li = xbmcgui.ListItem(matchObj[0] , iconImage='DefaultFolder.png')
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=matchObj[0], listitem=li)	
-	
-	
+		
 	# if matchobj:
 		# #xbmc.player().play(item=matchobj[0])		
 		# url = matchobj[0]
