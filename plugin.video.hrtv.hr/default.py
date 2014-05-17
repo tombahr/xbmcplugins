e5dd1e3c6ef5ae074878483fcde8223760d31028
tombahr <trajnovic@gmail.com>
2014-05-17 22:36:47 +0200

    Update default.py

--- plugin.video.hrtv.hr/default.py
+++ plugin.video.hrtv.hr/default.py
@@ -26,21 +26,25 @@ if mode is None:
 	url = build_url({'mode': 'rtl', 'foldername': 'RTL sada', 'link':'/rtl-sada'})
 	li = xbmcgui.ListItem('RTL sada', iconImage='DefaultFolder.png')
 	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)	
+	
+	url = build_url({'mode': 'htv', 'foldername': 'HTV na zahtjev', 'link':'http://www.hrt.hr/enz', 'level':0})
+	li = xbmcgui.ListItem('HTV na zahtjev', iconImage='DefaultFolder.png')
+	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)	
 	xbmcplugin.endOfDirectory(addon_handle)
 	
 # RTL sada
-
 elif mode[0] == 'rtl':
 	foldername = args['foldername'][0]
 	link = args['link'][0]
 	
 	###1 folder
 	#link = '/rtl-sada'
-	pat = r'<a href="('+link+'/[\w-]*)/" title="([\w\s-]*)">.*</a>'
+	pat = r'<a href="('+link+'/[^/]*)/" title="([^"]*)">.*</a>'
 	doc = urllib.urlopen(rtllink+link).read()
 	nl = re.findall( pat, doc, re.I)
 	
 	nl = list(set(nl))
+	nl.sort()
 	#print matchobj
 	for n in nl:
 		url = build_url({'mode': 'rtl', 'foldername': n[1], 'link': n[0]})
@@ -50,6 +54,7 @@ elif mode[0] == 'rtl':
 	#2 xml
 	pat = '<a href="('+link+'/[\d]*/.*/)" title="(.*)">'
 	nl = re.findall( pat, doc, re.I)
+	nl.sort()
 	for n in nl:
 		url = build_url({'mode': 'rtlxml', 'foldername': n[1], 'link': n[0]+"?xml=1"})
 		li = xbmcgui.ListItem(n[1], iconImage='DefaultFolder.png')
@@ -78,6 +83,45 @@ elif mode[0] == 'rtlxml':
     
 	xbmcplugin.endOfDirectory(addon_handle)
 
+#HTV enz
+elif mode[0] == 'htv':
+	level = int(args['level'][0])	
+	foldername = args['foldername'][0]
+	link = args['link'][0]
+	pat = ''
+	folder = True
+	if level==0: #lista emsija
+		pat = '<a href="('+link+'/[\w-]*)/">(.*)</a>'		
+	elif level==1: #lista epizoda
+		pat = r'<option value="(\d*)">([^<]*)' #(.*)</li>'
+	elif level ==2: #epizoda
+		pat= '<video data-url="([^"]*)"'
+	
+	doc = urllib.urlopen(link).read()
+	nl = re.findall( pat, doc, re.I)
+	nl = list(set(nl))
+	nl.sort()
+	olink=link
+	for n in nl:
+		isFolder = True
+		name = foldername
+		if level==1:
+			link=olink+n[0]			
+			name=n[1]
+		elif level==0:			
+			link=n[0]+'/'
+			name=n[1]
+		else:
+			link = n
+			name = foldername
+			isFolder=False
+		url = build_url({'mode': 'htv', 'foldername': name, 'link': link, 'level':level+1})
+		if level == 2:
+			url = link
+		li = xbmcgui.ListItem(name, iconImage='DefaultFolder.png')
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=isFolder)
+	
+	xbmcplugin.endOfDirectory(addon_handle)
 	
 elif mode[0] == 'rtlxmlll':
     foldername = args['foldername'][0]
